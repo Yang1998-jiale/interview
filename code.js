8639002d0db2d11d1278a0af8c31816f2e111756
yjl <2018770090@qq.com>
2024-05-15 16:30:47 +0800

    新增代码题

--- code.js
+++ code.js
@@ -2,7 +2,7 @@
  * @Author: yjl
  * @Date: 2024-05-08 17:47:16
  * @LastEditors: yjl
- * @LastEditTime: 2024-05-10 17:55:36
+ * @LastEditTime: 2024-05-15 16:30:04
  * @Description: 描述
  */
 /**
@@ -128,25 +128,12 @@ function query(data) {
   return new Data(data);
 }
 
-// function myTimeout(cbk, ms) {
-//   if (!ms || ms < 500) {
-//     ms = 500;
-//   }
-//   let start = Date.now();
-//   let intervalID = setInterval(() => {
-//     let now = Date.now();
-//     let time = now - start;
-//     if (time >= ms) {
-//       clearInterval(intervalID);
-//       cbk();
-//     }
-//   }, 1000);
-// }
-
-// myTimeout(() => {
-//   console.log("我执行了");
-// }, 3000);
-
+/**
+ * 简易的封装timeout
+ * @param {*} time 
+ * @param {*} ms 
+ * @param {*} cbk 
+ */
 function myTimeout(time, ms = 1000, cbk) {
   let start = +new Date();
   let timeID = null;
@@ -172,6 +159,9 @@ myTimeout(10000, 1000, () => {
   console.log("我执行了");
 });
 
+/**
+ * 返回合大于目标的元素
+ */
 let arrList = [
   { age: 20, name: "张三" },
   { age: 21, name: "李四" },
@@ -195,3 +185,143 @@ function queryMaxTarget(num, target) {
   return "所有合都小于num";
 }
 // console.log(queryMaxTarget(60, arrList));
+
+//event bus||订阅发布模式
+
+class EventBus {
+  list = new Map();
+  on(event, cbk) {
+    if (this.list.has(event)) {
+      this.list.get(event).push(cbk);
+    } else {
+      this.list.set(event, [cbk]);
+    }
+  }
+
+  emit(event, ...args) {
+    if (this.list.has(event)) {
+      this.list.get(event).forEach((cbk) => {
+        cbk.apply(this, args);
+      });
+    }
+  }
+}
+
+let eventBus = new EventBus();
+eventBus.on("aaa", (a) => {
+  console.log("哈哈哈");
+  console.log("我是cbk1", a);
+});
+eventBus.on("aaa", (a) => {
+  console.log("哈哈哈");
+  console.log("我是cbk2", a);
+});
+eventBus.emit("aaa", "123");
+
+//给str添加一个方法可以翻转
+String.prototype.reserveStr = function () {
+  return this.split("").reverse().join("");
+};
+let str = "123";
+
+console.log(str.reserveStr());
+
+//防抖
+function debounce(fn, time) {
+  let timeID = null;
+  return function () {
+    if (timeID) {
+      clearTimeout(timeID);
+    }
+    timeID = setTimeout(() => {
+      fn.apply(this, arguments);
+    }, time);
+  };
+}
+
+//节流
+function throttle(fn, time) {
+  let flag = true;
+  let timeID = null;
+  return function () {
+    if (!flag) return;
+    flag = false;
+    if (timeID) {
+      clearTimeout(timeID);
+    }
+    timeID = setTimeout(() => {
+      fn.apply(this, arguments);
+      flag = true;
+    }, time);
+  };
+}
+
+function deepClone(target, hash = new WeakMap()) {
+  if (target === null || typeof target !== "object") {
+    return target;
+  }
+
+  //判断hash中是否有缓存 有的话返回缓存
+  if (hash.has(target)) {
+    return hash.get(target);
+  }
+
+  let newTarget = {};
+  hash.set(target, newTarget);
+  let keys = Object.keys(target);
+  keys.forEach((key) => {
+    let value = target[key];
+    if (value === null || typeof value !== "object") {
+      newTarget[key] = value;
+    } else if (Array.isArray(value)) {
+      newTarget[key] = value.map((item) => deepClone(item, hash));
+    } else if (value instanceof Set) {
+      newTarget[key] = new Set([...value]);
+    } else if (value instanceof Map) {
+      newTarget[key] = new Map([...value]);
+    } else {
+      newTarget[key] = deepClone(value, hash);
+    }
+  });
+
+  return newTarget;
+}
+
+
+//观察者模式
+class Subject {
+  constructor() {
+    this.list = [];
+  }
+  addObs(obs) {
+    this.list.push(obs);
+  }
+
+  removeObs(obs) {
+    this.list = this.list.filter((item) => item !== obs);
+  }
+
+  notify(text) {
+    this.list.forEach((obs) => {
+      obs.update(text);
+    });
+  }
+}
+
+class Observer {
+  constructor(name, update) {
+    this.name = name;
+    this.update = update;
+  }
+}
+
+let subject = new Subject();
+let obs1 = new Observer("obs1", (text) => {
+  console.log("我是观察1", text);
+});
+let obs2 = new Observer("obs2", (text) => {
+  console.log("我是观察2", text);
+});
+subject.addObs(obs1);
+subject.addObs(obs2);
+subject.notify("123");
